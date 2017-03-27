616fe0ab6cf4f2964035bb8bfce5dadb623171c6
belle05 <cbfe510@gmail.com>
2017-03-28 00:44:55 +0300

    finished code, not yet bug-less

--- main.c
+++ main.c
@@ -6,22 +6,59 @@
  */
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 
-int convert210(char word[],char dict[],int myBase) {
-	int sum = 0;
-	for (int i = 0; i<sizeof(word); i++){
-		for (int l = 0; l<sizeof(dict);l++) {
+int int_pow(int base, int exp) {
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+           result *= base;
+        exp /= 2;
+        base *= base;
+    }
+    return result;
+}
+
+int convert210(char *word, char *dict, int myBase) {
+	int sum, pow;
+	for (unsigned int i = 0; i<sizeof(word); i++) {
+		for (unsigned int l = 0; l<sizeof(&dict); l++) {
 			if (word[i]==dict[l]) {
-				sum += l*(pow(myBase,i));
+				pow = int_pow(myBase, i);
+				sum += l * pow;
 			}
 		}
 	}
 	return sum;
 }
-void main()
+
+void convertFrom10(int sum, char *dict, int newBase, char *result){
+	int i, j, index;
+	char new_num[20];
+	char tempChar;
+	while (sum!=1) {
+		tempChar = dict[sum%newBase];
+		new_num[index] = tempChar;
+		index++;
+		sum = sum / newBase;
+	}
+
+	//reversing the array
+	j = sizeof(new_num)-1;
+	while (i < j) {
+		tempChar = new_num[i];
+	    new_num[i] = new_num[j];
+	    new_num[j] = tempChar;
+	    i++;
+	    j--;
+	}
+	sprintf(result, "%s", new_num);
+}
+
+int main()
 {
-	char dict[] = "0123456789ABCDEF";
 	char dictA[] = "0123456789ABCDEF";
 	char dictB[] = "0123456789ABCDEF";
 	int baseA, baseB,sum;
@@ -30,41 +67,56 @@ void main()
 	char word[20];
 	char result[20];
 	char Char;
+
+	//Get first base
 	printf ("Please enter the number's base:\n");
-	scanf ("%d", baseA);
-	if(baseA>16 && baseA < 2) {
+	scanf ("%d", &baseA);
+	if(baseA>16 || baseA < 2) {
 		printf ("Invalid input base\n");
+		return 0;
 	}
-	dictA[baseA] = '\0';
+
+	//Get second base
 	printf ("Please enter the desired base:\n");
-	scanf ("%d", baseB);
+	scanf ("%d", &baseB);
 	if(baseB>16 && baseB < 2) {
 		printf ("Invalid input base\n");
+		return 0;
 	}
-	dictB[baseB] = '\0';
-	printf ("Please enter a number in base %d:\n",baseA);
-	while ((Char = getchar() != EOF)) {
-		for (int i=0; i<sizeof(dictA); i++){
-			if (Char==dictA[i]){
+
+	//Get number to convert
+	printf ("Please enter a number in base %d:\n", baseA);
+	Char = getchar();
+	while ((Char = getchar()) != EOF) {
+		for (int i=0; i<baseA; i++) {
+			if (Char==dictA[i]) {
+				//printf("char is not %c\n", dictA[i]);
 				found = 1;
+				i = baseA;
 			}
 		}
-		if (found == 0){
+		if (found == 0) {
 			printf ("Invalid number!\n");
 		}
-
 		word[index] = Char;
 		index++;
 	}
+
+	//convert to decimal base if needed
 	if (baseA!=10) {
 		sum = convert210(word,dictA,baseA);
 	} else {
 		sum = sscanf(word,"%d",&sum);
 	}
 
+	//convert from decimal to baseB if needed
+	if (baseB!=10) {
+		convertFrom10(sum, dictB, baseB, result);
+	} else {
+		sprintf(result, "%d", sum);
+	}
 
-
-	printf ("The result is : %s\n",result);
-
+	//print result
+	printf ("The result is : %s\n", result);
 
 }
