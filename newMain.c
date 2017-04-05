ec0e9340582189f2b22dc73aca6f0216e8d61e2e
nufarlevy2 <levynufar@gmail.com>
2017-04-05 22:47:49 +0300

    והווו נשאר לבדוק רק מקרי קצה, תראי שם מה שיניתי היה לך שם טעות קטנה שזה נעצר ב1 במקום ב0 בwhile חוצמיזה זה היה שטויות

--- newMain.c
+++ newMain.c
@@ -1,4 +1,4 @@
-  /*
+ /*
  *  Created on: Mar 27, 2017
  *      Author: nlevy
  */
@@ -20,35 +20,35 @@ int int_pow(int base, int exp) {
     return result;
 }
 
-int convert210(char *word, char *dict, int myBase){
-        int sum=0;
-        int pow;
-        for (unsigned int i = 0; i<sizeof(word); i++) {
-                for (int l = 0; l<myBase; l++) {
-                        char charA = word[i];
-                        char charB = dict[l];
+//int convert210(char tempChar, char *dict, int myBase){
+   //     int sum = 0;
+  //      int pow;
+ //       for (int l = 0; l<myBase; l++) {
+          //              char charB = dict[l];
                 //      printf("char is %c\n",charA);
                 //      printf("dict is %c\n",charB);
-                       // printf("i is %d\n", i);
-                       // printf("l is %d\n" ,l);
-                        if (charA == charB) {
-                                pow = int_pow(myBase, i);
-                               // printf("pow is %d\n", pow);
-                                sum += l * pow;
-                               // printf("sum is %d\n", sum);
-                        }
-                }
-        }
-        return sum;
-}
+           //            // printf("i is %d\n", i);
+          //             // printf("l is %d\n" ,l);
+         //               if (tempChar == charB) {
+        //                        pow = int_pow(myBase, i);
+       //                        // printf("pow is %d\n", pow);
+      //                          sum += l * pow;
+     //                          // printf("sum is %d\n", sum);
+    //                    }
+   //             }
+  //      }
+ //       return sum;
+//}
 
 void convertFrom10(int sum, char *dict, int newBase, char *result){
         int i, j, index;
+        index = 0;
+        int end;
         char new_num[20];
         char tempChar;
-        while (sum!=1) {
+		while (sum!=0) {
                 tempChar = dict[sum%newBase];
-                printf("temp char %c\n",tempChar);
+//                printf("temp char %c\n",tempChar);
                 new_num[index] = tempChar;
                 index++;
                 sum = sum / newBase;
@@ -57,7 +57,11 @@ void convertFrom10(int sum, char *dict, int newBase, char *result){
         //reversing the array
         for (unsigned int i = 0; i<sizeof(new_num);i++) {
                 if (isdigit(new_num[i]) || new_num[i] == 'A' || new_num[i] == 'B' ||  new_num[i] == 'C' ||  new_num[i] == 'D' ||  new_num[i] == 'E' ||  new_num[i] == 'F'   ) {
+                end = i;
                 j = i;
+                }
+                else {
+                      new_num[i] = '\0';
                 }
         }
         i = 0;
@@ -68,8 +72,8 @@ void convertFrom10(int sum, char *dict, int newBase, char *result){
                 i++;
                 j--;
         }
-        for (unsigned int i = 0;i<sizeof(new_num);i++) {
-                printf("result: %c\n",new_num[i]);
+        for (int i = 0;i<end+1;i++) {
+  //              printf("result: %c\n",new_num[i]);
         }
         sprintf(result, "%s", new_num);
 }
@@ -83,9 +87,10 @@ int main()
         int baseA, baseB,sum;
         int found = 0;
         int index = 0;
-        char word[20];
         char result[20];
         char Char;
+        char myChar;
+
         //Get first base
         printf ("Please enter the number's base:\n");
         scanf ("%d", &baseA);
@@ -101,7 +106,8 @@ int main()
                 printf ("Invalid input base\n");
                 return 0;
         }
-
+        sum = 0;
+        index = 0;
         //Get number to convert
         printf ("Please enter a number in base %d:\n", baseA);
         Char = getchar();
@@ -110,51 +116,52 @@ int main()
                         if (Char==dictA[i]) {
                                 //printf("char is not %c\n", dictA[i]);
                                 found = 1;
+                                myChar = i;
                                 i = baseA;
                         }
                 }
                 if (found == 0) {
                         printf ("Invalid number!\n");
+                        break;
                 }
-                word[index] = Char;
+                if (index!=0) {
+                        sum = sum*baseA;
+                }
+                sum += myChar;
                 index++;
         }
-        int i = 0;
-        char tempChar;
-        int j;
-        char rWord[20];
+  //      int i = 0;
+ //       char tempChar;
+  //      int j;
+   //     char rWord[20];
 //reversing the array
-        for (unsigned int i = 0; i<sizeof(word);i++) {
-                if (isdigit(word[i]) || word[i] == 'A' || word[i] == 'B' || word[i] == 'C' || word[i] == 'D' || word[i] == 'E' || word[i] == 'F' ) {
-                        j = i;
-                        rWord[i] = word[i];
-                }
-                else {
-                        word[i] = '\0';
-                        rWord[i] = '\0';
-                }
-        }
-        while (i < j) {
-                tempChar = rWord[i];
-                rWord[i] = rWord[j];
-                rWord[j] = tempChar;
-                i++;
-                j--;
-        }
+   //     for (unsigned int i = 0; i<sizeof(word);i++) {
+             //   if (isdigit(word[i]) || word[i] == 'A' || word[i] == 'B' || word[i] == 'C' || word[i] == 'D' || word[i] == 'E' || word[i] == 'F' ) {
+            //            j = i;
+           //             rWord[i] = word[i];
+          //      }
+         //       else {
+        //                word[i] = '\0';
+       //                 rWord[i] = '\0';
+      //          }
+     //   }
+      //  while (i < j) {
+     //           tempChar = rWord[i];
+    //            rWord[i] = rWord[j];
+   //             rWord[j] = tempChar;
+  //              i++;
+ //               j--;
+//        }
 
-
-        //convert to decimal base if needed
-        sum = convert210(rWord,dictA,baseA);
 
         //convert from decimal to baseB if needed
         if (baseB!=10) {
                 convertFrom10(sum, dictB, baseB, result);
-        } else {
-                printf("base is 10 and sum is %d\n", sum);
-                sprintf(result, "%d", sum);
+                printf("The result is : %s\n", result);
+        }
+        else {
+                printf("The result is : %d\n", sum);
         }
 
-        //print result
-        printf ("The result is : %s\n", result);
 }
 
